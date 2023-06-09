0d7bd91104ffdac5b5f3f4cb0335a47616010455
Andyvpn <133840370+Andyvpn@users.noreply.github.com>
2023-06-09 16:58:40 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -20,7 +20,7 @@ commonname=none
 email=agunxz@themoon.my,id
 
 # simple password minimal
-wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/andristji/Xray-SSH/main/password.txt"
+wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/Andyvpn/Autoscript-vpn/main/password.txt"
 chmod +x /etc/pam.d/common-password
 
 # go to root
