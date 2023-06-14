7c0d126e7e1bba2b15b39205a7a242a72479e8bd
Andyvpn <133840370+Andyvpn@users.noreply.github.com>
2023-06-15 00:24:41 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -136,13 +136,13 @@ mkdir -p /home/vps/public_html
 # install badvpn
 cd
 cd
-wget -O /usr/bin/badvpn-udpgw https://github.com/Andyvpn/Autoscript-vpn/main/badvpn/badvpn-udpgw && chmod +x  /usr/bin/badvpn-udpgw
+wget -O /usr/bin/badvpn-udpgw https://raw.github.com/Andyvpn/Autoscript-by-azi/main/udpgw/badvpn-udpgw && chmod +x  /usr/bin/badvpn-udpgw
 #system badvpn 7300
-wget -O /etc/systemd/system/svr-7300.service https://github.com/Andyvpn/Autoscript-vpn/main/badvpn/svr-7300.service && chmod +x  /etc/systemd/system/svr-7300.service
+wget -O /etc/systemd/system/svr-7300.service https://raw.github.com/Andyvpn/Autoscript-by-azi/main/badvpn/svr-7300.service && chmod +x  /etc/systemd/system/svr-7300.service
 #system badvpn 7200
-wget -O /etc/systemd/system/svr-7200.service https://github.com/Andyvpn/Autoscript-vpn/main/badvpn/svr-7200.service && chmod +x  /etc/systemd/system/svr-7200.service
+wget -O /etc/systemd/system/svr-7200.service https://raw.github.com/Andyvpn/Autoscript-by-azi/main/badvpn/svr-7200.service && chmod +x  /etc/systemd/system/svr-7200.service
 #system badvpn 7100
-wget -O /etc/systemd/system/svr-7100.service https://github.com/Andyvpn/Autoscript-vpn/main/badvpn/svr-7100.service && chmod +x  /etc/systemd/system/svr-7100.service
+wget -O /etc/systemd/system/svr-7100.service https://raw.github.com/Andyvpn/Autoscript-by-azi/main/badvpn/svr-7100.service && chmod +x  /etc/systemd/system/svr-7100.service
 
 #reboot system 7100
 systemctl daemon-reload
